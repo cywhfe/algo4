9475b2cb33db1a420a84bb9a6d9f28d0577ac2e0
cywhfe <cywhfe@hotmail.com>
2017-09-17 00:25:45 +0800

    string compare parts

--- src/main/java/cy/chapter5_1/LSD.java
+++ src/main/java/cy/chapter5_1/LSD.java
@@ -12,30 +12,28 @@ public class LSD {
 
     public static void sort(String[] a, int w) {
         int n = a.length;
-        int R = 256;
+        int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
-        for (int d = w - 1; d >= 0; d--) {
+        for (int d = w-1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
 
-            int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++) {
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = 0; i < n; i++)
                 count[a[i].charAt(d) + 1]++;
-            }
 
-            //compute cumulates
-            for (int r = 0; r < R; r++) {
-                count[r + 1] += count[r];
-            }
+            // compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
 
-            //move date
-            for (int i = 0; i < n; i++) {
+            // move data
+            for (int i = 0; i < n; i++)
                 aux[count[a[i].charAt(d)]++] = a[i];
-            }
 
-            //copy back
-            for (int i = 0; i < n; i++) {
+            // copy back
+            for (int i = 0; i < n; i++)
                 a[i] = aux[i];
-            }
         }
     }
 
